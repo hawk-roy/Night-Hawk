64400e3045907caa831ab09980cbf628e3d993d7
hawk-roy <teng_lei_maker@163.com>
2026-06-09 00:20:18 +0800

    fix: remove nousage code

--- internal/handler/product_handler.go
+++ internal/handler/product_handler.go
@@ -4,43 +4,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/hawk-roy/Night-Hawk/internal/model"
 	"github.com/hawk-roy/Night-Hawk/internal/repository"
 )
-
-var orderProducts = []model.Product{
-	{
-		ID:          1,
-		Name:        "Go Backend Course",
-		Description: "A practical Go backend course",
-		Price:       19900,
-		Stock:       100,
-	},
-	{
-		ID:          2,
-		Name:        "API Design Handbook",
-		Description: "A handbook for designing backend APIs",
-		Price:       9900,
-		Stock:       50,
-	},
-	{
-		ID:          3,
-		Name:        "Cloud Native Starter Kit",
-		Description: "A starter kit for cloud native applications",
-		Price:       29900,
-		Stock:       30,
-	},
-}
-
-func GetProductByID(productID int64) (*model.Product, bool) {
-	for i := range orderProducts {
-		if orderProducts[i].ID == productID {
-			return &orderProducts[i], true
-		}
-	}
-
-	return nil, false
-}
 
 func ListProducts(productRepo *repository.ProductRepository) gin.HandlerFunc {
 	return func(c *gin.Context) {
